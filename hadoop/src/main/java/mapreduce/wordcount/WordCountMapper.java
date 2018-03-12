efda3be7d50888b55d346adc9c5c12dc8b42696c
chongaizhen <18363999928@163.com>
2018-03-12 23:06:50 +0800

    新建wordcount

--- hadoop/src/main/java/mapreduce/wordcount/WordCountMapper.java
+++ hadoop/src/main/java/mapreduce/wordcount/WordCountMapper.java
@@ -1,9 +1,18 @@
 package mapreduce.wordcount;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
 
 /**
  * Created by chongaizhen on 2018/03/12.
  */
-public class WordCountMapper extends Mapper {
+public class WordCountMapper extends Mapper<LongWritable,Text,Text,LongWritable> {
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+    }
 }
--- hadoop/src/main/java/mapreduce/wordcount/WordCountReducer.java
+++ hadoop/src/main/java/mapreduce/wordcount/WordCountReducer.java
@@ -1,7 +1,18 @@
 package mapreduce.wordcount;
 
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
 /**
  * Created by chongaizhen on 2018/03/12.
  */
-public class WordCountReducer {
+public class WordCountReducer extends Reducer<Text,LongWritable,Text,LongWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+
+    }
 }
