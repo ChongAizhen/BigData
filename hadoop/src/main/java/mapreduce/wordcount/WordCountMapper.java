489d60153f4a446d7e4859eac5cfaa3c53ea816a
chongaizhen <18363999928@163.com>
2018-03-12 22:57:09 +0800

    添加部分依赖

---
+++ hadoop/src/main/java/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,9 @@
+package mapreduce.wordcount;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ * Created by chongaizhen on 2018/03/12.
+ */
+public class WordCountMapper extends Mapper {
+}
---
+++ hadoop/src/main/java/mapreduce/wordcount/WordCountReducer.java
@@ -0,0 +1,7 @@
+package mapreduce.wordcount;
+
+/**
+ * Created by chongaizhen on 2018/03/12.
+ */
+public class WordCountReducer {
+}
