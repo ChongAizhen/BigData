5cfa93d85261f57bd2b6bc9d98423e0c35513022
chongaizhen <18363999928@163.com>
2018-03-22 23:41:11 +0800

    给定一个文本，计算文本的总长度

---
+++ spark/src/main/scala/rdd/TotalLenth.scala
@@ -0,0 +1,35 @@
+package rdd
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by chongaizhen on 2018/03/22.
+  */
+
+//给定一个文本，计算文本的总长度
+object TotalLenth {
+  def main(args: Array[String]): Unit = {
+    /*
+    java版：
+    JavaRDD<String> lines = sc.textFile("hdfs://master:9000/testFile/README.md");
+
+    //第一个参数为传入的内容，第二个参数为函数操作完后返回的结果类型
+    JavaRDD<Integer> lineLengths = lines.map(new Function<String, Integer>() {
+    public Integer call(String s) {
+        System.out.println("每行长度"+s.length());
+        return s.length(); }
+    });
+
+    int totalLength = lineLengths.reduce(new Function2<Integer, Integer, Integer>() {
+        public Integer call(Integer a, Integer b) { return a + b; }
+    });
+
+    System.out.println(totalLength);
+     */
+    val conf = new SparkConf().setAppName("TotalLenth").setMaster("local");
+    val sc = new SparkContext(conf);
+    val rdd1=sc.textFile("E:\\IdeaProjects\\git\\BigData\\data\\input\\test1").map(_.length).reduce(_+_)
+    println(rdd1)
+
+  }
+}
--- spark/src/main/scala/rdd/WordCount.scala
+++ spark/src/main/scala/rdd/WordCount.scala
@@ -9,7 +9,20 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("WC").setMaster("local");
     val sc = new SparkContext(conf);
-    val rdd1 = sc.textFile("E:\\IdeaProjects\\git\\BigData\\data\\input\\test1").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    val rdd1 = sc.textFile("E:\\IdeaProjects\\git\\BigData\\data\\input\\test1")
+      /*
+       data 有两行数据，第一行 a,b,c，第二行1,2,3
+       scala>data.map(line1 => line1.split(",")).collect()
+       res11: Array[Array[String]] = Array(Array(a, b, c),Array(1, 2, 3))
+       scala>data.flatMap(line1 => line1.split(",")).collect()
+       res12: Array[String] = Array(a, b, c, 1, 2, 3)
+       */
+      .flatMap(_.split(" "))
+      .map((_, 1))
+      //reduceByKey(x,y=>x+y)
+      .reduceByKey(_ + _)
+      //按照第二个值的大小降序排序
+      .sortBy(_._2,false)
       .saveAsTextFile("E:\\IdeaProjects\\git\\BigData\\data\\output")
   }
 }
