d98680fbc757c0d6c16c3d3c0952c9d6fb917130
chongaz0717 <chongaz0717@thundersoft.com>
2018-03-23 17:00:13 +0800

    保存数据到cassandra

--- spark/src/main/scala/rdd/Cassandra.scala
+++ spark/src/main/scala/rdd/Cassandra.scala
@@ -11,16 +11,18 @@ object Cassandra {
     val conf = new SparkConf()
       .set("spark.cassandra.connection.host", "192.168.7.206")
       .set("spark.cassandra.connection.port", "9042")
-      .setAppName("jsonParser").setMaster("local")
+      .setAppName("cassandra").setMaster("local")
     val sc = new SparkContext(conf)
 
     //读取上一次的时间戳
     val ts:Long = sc.textFile("hdfs://localhost:9000/result/part-00000").collect()(0).toLong
     println(ts)
 
-    val rdd = sc.cassandraTable("thingsboard", "ts_kv_cf").where(" ts > ?",ts)
+    val rdd = sc.cassandraTable("cassandra", "ts_kv_cf").where(" ts > ?",ts)
 
     rdd.collect().foreach(row => println(s"Existing Data: $row"))
+
+    rdd.saveToCassandra("bigdata", "spark", SomeColumns("id", "ts", "value"))
 
     sc.stop()
   }
