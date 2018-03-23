7bfea7431c59d9cac635229675bc17faec6861c1
chongaz0717 <chongaz0717@thundersoft.com>
2018-03-23 16:58:26 +0800

    新增从cassandra获取数据源

---
+++ spark/src/main/scala/rdd/Cassandra.scala
@@ -0,0 +1,27 @@
+package rdd
+
+import org.apache.spark.{SparkConf, SparkContext}
+import com.datastax.spark.connector._
+
+/**
+  * Created by Chong AiZhen on 18-3-23,下午4:50. 
+  */
+object Cassandra {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+      .set("spark.cassandra.connection.host", "192.168.7.206")
+      .set("spark.cassandra.connection.port", "9042")
+      .setAppName("jsonParser").setMaster("local")
+    val sc = new SparkContext(conf)
+
+    //读取上一次的时间戳
+    val ts:Long = sc.textFile("hdfs://localhost:9000/result/part-00000").collect()(0).toLong
+    println(ts)
+
+    val rdd = sc.cassandraTable("thingsboard", "ts_kv_cf").where(" ts > ?",ts)
+
+    rdd.collect().foreach(row => println(s"Existing Data: $row"))
+
+    sc.stop()
+  }
+}
