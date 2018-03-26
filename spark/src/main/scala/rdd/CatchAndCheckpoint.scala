937c9f5769f571932c591b6fe1f571c1c53faa10
chongaz0717 <chongaz0717@thundersoft.com>
2018-03-26 11:06:36 +0800

    更新checkpoint操作

--- spark/src/main/scala/rdd/CatchAndCheckpoint.scala
+++ spark/src/main/scala/rdd/CatchAndCheckpoint.scala
@@ -9,12 +9,12 @@ object CatchAndCheckpoint {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("CatchAndCheckpoint").setMaster("local");
     val sc = new SparkContext(conf);
-    val rdd = sc.textFile("").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).sortByKey()
+    val rdd = sc.textFile("/home/user/IdeaProjects/github/BigData/data/input/test1").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).sortByKey()
     rdd.cache()
-    sc.setCheckpointDir("checkpoint存放的目录")
+    sc.setCheckpointDir("/home/user/IdeaProjects/github/BigData/data/checkpoint")
     rdd.checkpoint()
-    rdd.collect()
-    rdd.saveAsTextFile("")
+    rdd.collect().foreach(println)
+    rdd.saveAsTextFile("/home/user/IdeaProjects/github/BigData/data/output")
     sc.stop()
   }
 }
