40397cbdc2d111c38185f86ca4b56a8b648b5853
chongaz0717 <chongaz0717@thundersoft.com>
2018-03-30 19:18:15 +0800

    不清楚lambda中怎么用ifelse

--- spark/src/main/scala/project/project1/src/MobileLocation.scala
+++ spark/src/main/scala/project/project1/src/MobileLocation.scala
@@ -10,8 +10,19 @@ object MobileLocation {
     val conf = new SparkConf().setAppName("MobileLocation").setMaster("local[2]")
     val sc = new SparkContext(conf)
 
-    val rdd1 = sc.textFile("E:\\IdeaProjects\\git\\BigData\\spark\\src\\main\\scala\\project\\project1\\data")
+    val rdd1 = sc.textFile("/home/user/IdeaProjects/github/BigData/spark/src/main/scala/project/project1/data/bs_log")
     println(rdd1.partitions.length)
+
+
+    val rdd2 = rdd1.flatMap(a => {
+      val arr = a.split(",")
+      if(arr(3).equals("1")){
+        val rdd = (arr(0)+"_"+arr(2),arr(1))
+      }else{
+        val rdd = (arr(0)+"_"+arr(2),arr(1))
+      }
+      a
+    })
 
     sc.stop()
   }
