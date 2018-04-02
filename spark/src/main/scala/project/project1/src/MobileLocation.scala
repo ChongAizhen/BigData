d86ef8a194bf99399631988b6ba532cc8ac041fc
chongaizhen <18363999928@163.com>
2018-04-02 23:44:37 +0800

    继续编写通过基站求用户一天中待的时间最长的两个小区

--- spark/src/main/scala/project/project1/src/MobileLocation.scala
+++ spark/src/main/scala/project/project1/src/MobileLocation.scala
@@ -10,19 +10,30 @@ object MobileLocation {
     val conf = new SparkConf().setAppName("MobileLocation").setMaster("local[2]")
     val sc = new SparkContext(conf)
 
-    val rdd1 = sc.textFile("/home/user/IdeaProjects/github/BigData/spark/src/main/scala/project/project1/data/bs_log")
+    val rdd1 = sc.textFile("E:\\IdeaProjects\\git\\BigData\\spark\\src\\main\\scala\\project\\project1\\data\\bs_log")
     println(rdd1.partitions.length)
-
 
     val rdd2 = rdd1.map(a => {
       val arr = a.split(",")
-      val rdd3 = if(arr(3).equals("1")){
-        (arr(0)+"_"+arr(2),arr(1))
+      val userAndTime = if(arr(3).equals("1")){
+        (arr(0)+"_"+arr(2),-arr(1).toLong)
       }else{
-        (arr(0)+"_"+arr(2),arr(1))
+        (arr(0)+"_"+arr(2),arr(1).toLong)
       }
-      rdd3
+      userAndTime
     })
+
+    val rdd3 = rdd2.reduceByKey(_+_)
+
+    rdd3.collect().foreach(println)
+
+    val rdd4 = rdd3.sortBy(_._2,false).groupBy(_._1.split("_")(0))
+
+    rdd4.collect().foreach(println)
+
+
+    val lac = sc.textFile("E:\\IdeaProjects\\git\\BigData\\spark\\src\\main\\scala\\project\\project1\\data\\lac_info.txt")
+    .map(_.split(",")).map(x => (x(0),x(1),x(2)))
 
     sc.stop()
   }
