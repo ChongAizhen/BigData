eea296abbcee35c65434cec08d2ef43103bafe37
chongaizhen <18363999928@163.com>
2018-04-03 23:37:10 +0800

    新增他人的解法，但是只能取到top1

--- spark/src/main/scala/project/project1/src/MobileLocation.scala
+++ spark/src/main/scala/project/project1/src/MobileLocation.scala
@@ -1,5 +1,7 @@
 package project.project1.src
 
+import java.text.SimpleDateFormat
+
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -27,10 +29,34 @@ object MobileLocation {
 
     rdd3.collect().foreach(println)
 
-    val rdd4 = rdd3.sortBy(_._2,false).groupBy(_._1.split("_")(0))
+    val rdd4 = rdd3.map(x=>(x._1.split("_")(0),x._1.split("_")(1),x._2))
 
     rdd4.collect().foreach(println)
 
+    val sdf = new SimpleDateFormat("yyyyMMddHHmmss")
+    val rdd9 = sc.textFile("E:\\IdeaProjects\\git\\BigData\\spark\\src\\main\\scala\\project\\project1\\data\\bs_log").map(_.split(",")).map(x => {
+      //（手机号_基站ID，时间，事件类型）
+      (x(0) + "_" + x(2), sdf.parse(x(1)).getTime, x(3))
+      //按 手机号_基站ID 分组
+    }).groupBy(_._1).mapValues(_.map(
+        //建立连接基站的时间设置为负的
+        x => if (x._3.toInt == 0) x._2.toLong else -x._2.toLong
+    )).mapValues(_.sum)
+
+    //rdd9与rdd3的格式相同
+    rdd9.collect().foreach(println)
+
+    val rdd10 = rdd9.groupBy(_._1.split("_")(0)).map { case (k, v) => {
+      //分组后二次排序
+      (k, v.toList.sortBy(_._2).reverse(0))
+    }
+    }
+
+    rdd10.collect().foreach(println)
+
+    val rdd11 = rdd10.map(t => (t._1, t._2._1.split("_")(1), t._2._2))
+
+    rdd11.collect().foreach(println)
 
     val lac = sc.textFile("E:\\IdeaProjects\\git\\BigData\\spark\\src\\main\\scala\\project\\project1\\data\\lac_info.txt")
     .map(_.split(",")).map(x => (x(0),x(1),x(2)))
